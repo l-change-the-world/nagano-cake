6cb1dc32334457db93155952d38a936344c915a6
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-05-18 01:05:54 +0000

    [Fix]Users model delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  # 会員側ルーティング
+  # 会員側deviseルーティング
   devise_for :members, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  # 管理者側ルーティング
+  # 管理者側deviseルーティング
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+
+  #会員側ルーティング
+
+
+  # 管理者側ルーティング
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
