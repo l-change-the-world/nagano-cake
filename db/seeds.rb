c54e809db03ef22ebcfb06bded13e4aad81a7451
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-05-21 01:47:20 +0000

    [Add]seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 Admin.create(email: "admin@co.jp", password: "naganocake", password_confirmation: "naganocake")
 
 # ここから仮のデータ。実装前に削除
-#Member.create(email:public@co.jp,last_name:"山田",first_name:"花子",last_name_ruby:"ヤマダ",first_name_ruby:"ハナコ",postal_code:address:telephone_number:is_deleted:)
-#Genre.create(name:"ケーキ")
-#Item.create(name:"チーズケーキ",description:"北海道産チーズ使用",genre_id: 1,price_excluding_tax: 1200,is_for_sale:true)
-#OrderItem.create(order_id: 1,item_id: 1,quantity: 1,price: 1,production_status: 0)
-#Order.create(postal_code:address:name:shipping_fee:payment_method:status:total_payment_amount)
+Member.create(email:"public@co.jp",password:"naganocake",encrypted_password:"naganocake", last_name:"山田", first_name:"花子", last_name_ruby:"ヤマダ", first_name_ruby:"ハナコ", postal_code:"0000000", address:"宮崎県", telephone_number:"00000000000", is_deleted:true)
+Genre.create(name:"ケーキ")
+Item.create(name:"チーズケーキ",description:"北海道産チーズ使用",genre_id: 1,price_excluding_tax: 1200,is_for_sale:true)
+OrderItem.create(order_id: 1,item_id: 1,quantity: 1,price: 1,production_status: 0)
+Order.create(postal_code:"1111111",address:"宮城県",name:"山田花子",shipping_fee:800,payment_method:0,status:0,total_payment_amount:2000)
