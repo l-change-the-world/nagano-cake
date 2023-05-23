e8c8f183cc6930130c75c6232018eb2cfcd7f60f
Yukari Hangai <129511468+Yukari-Hangai@users.noreply.github.com>
2023-05-23 08:46:29 +0000

    商品情報入力・確認画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -14,17 +14,18 @@ class Public::OrdersController < ApplicationController
   end
   
   def confirm #注文情報入力確認画面
+     @cart_items = current_member.shopping_cart_items.all
     @order = Order.new(order_params)
-    if params[:order][:select_address] == 0
+    if params[:order][:select_address] == "0"
       @order.postal_code = current_member.postal_code
       @order.address = current_member.address
       @order.name = current_member.last_name + current_member.first_name
-    elsif params[:order][:select_address] == 1
+    elsif params[:order][:select_address] == "1"
       ship = ShippingAddresses.find(params[:order][:member_id])#orderのmember_id(=カラム)でアドレス(帳)を選び、そのデータ送る
       @order.postal_code = ship.postal_code
       @order.address = ship.address
       @order.name = ship.name
-    elsif params[:order][:select_address] == 2
+    elsif params[:order][:select_address] == "2"
       @order.postal_code = params[:order][:postal_code]
       @order.address = params[:order][:address]
       @order.name = params[:order][:name]
