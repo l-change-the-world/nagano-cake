93974a47b39356b4c75709e5f755d17e9eff7dc3
EC2 Default User <ec2-user@ip-172-31-6-132.ap-northeast-1.compute.internal>
2023-05-25 07:51:32 +0000

    FIX

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy #中間テーブル
   has_many :items, through: :ordered_items
   belongs_to :member
-  
+
   # 注文ステータス (0：入金待ち、1：入金確認、2：制作中、3：発送準備中、4：発送済み)
   enum status: {
     waiting: 0,
@@ -11,15 +11,15 @@ class Order < ApplicationRecord
     preparing: 3,
     shipped: 4,
   }
-  
+
   # 支払方法
   enum payment_method: {
     credit_card: 0,
     transfer: 1
   }
-  
+
   def subtotal
     item.with_tax_price * quantity
   end
-  
+
 end
