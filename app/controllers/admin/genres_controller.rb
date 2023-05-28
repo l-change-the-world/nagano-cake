515954bae5ac928f03413596be72b3493c11d2e1
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-05-28 13:56:18 +0000

    [Add]README

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -9,32 +9,32 @@ class Admin::GenresController < ApplicationController
       render 'index'
     end
   end
-  
+
   def index
     @genre = Genre.new
     @genres = Genre.all
   end
-  
+
   def edit
     @genre = Genre.find(params[:id])
   end
-  
+
   def update
     genre = Genre.find(params[:id])
     genre.update(genre_params)
     redirect_to admin_genres_path
   end
-  
+
   def destroy
     genre = Genre.find(params[:id])  # データ（レコード）を1件取得
     genre.destroy  # データ（レコード）を削除
-    redirect_to admin_genres_path  # 投稿一覧画面へリダイレクト  
+    redirect_to admin_genres_path  # 投稿一覧画面へリダイレクト
   end
-  
+
   private
-  
+
   def genre_params
     params.require(:genre).permit(:name)
   end
-  
+
 end
